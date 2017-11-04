3a081dcbb16560f0b07d108a999f640b9a4460f7
Fabio Carpinato <fabiocarpinato@outlook.it>
2017-11-04 13:22:15 +0100

    added die and dump function

--- functions/functions.php
+++ functions/functions.php
@@ -3,3 +3,9 @@
 function is_adult($age){
     return $age >= 21;
 }
+
+function dd($data){
+    echo '<pre>';
+    die(var_dump($data));
+    echo '</pre>';
+}
--- functions/index.php
+++ functions/index.php
@@ -3,10 +3,12 @@
 require 'functions.php';
 
 $age = 20;
-
+$ages = [ 30 , 50 , 44 ,'cinquanta'];
 
 if(is_adult($age)){
     echo 'You can come in!';
 }else{
     echo 'You can\'t come in!';
 }
+
+dd($ages);
